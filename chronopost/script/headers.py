76f4f9c882a56fe2b165fa8f7b4fb08f91739e22
TonNom <tonemail@email.com>
2026-01-28 00:30:20 +0100

    d

--- chronopost/script/headers.py
+++ chronopost/script/headers.py
@@ -1,81 +1,41 @@
 iv4 = "d8731416d5d60aac657dd0120cc49f59"
 
 HEADERS_1 = {
-    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-    "accept-encoding": "gzip, deflate, br, zstd",
-    "accept-language": "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7",
-    "cache-control": "max-age=0",
-    "priority": "u=0, i",
-    "referer": "https://www.chronopost.fr/expeditionAvanceeSec/accueilShipping.do?_=1769545289869&lang=fr_FR",
-    "sec-ch-ua": "\"Not(A:Brand\";v=\"8\", \"Chromium\";v=\"144\", \"Google Chrome\";v=\"144\"",
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": "\"Windows\"",
-    "sec-fetch-dest": "document",
-    "sec-fetch-mode": "navigate",
-    "sec-fetch-site": "same-origin",
-    "sec-fetch-user": "?1",
-    "upgrade-insecure-requests": "1",
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:146.0) Gecko/20100101 Firefox/146.0",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language": "fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
+    "Referer": "https://www.chronopost.fr/fr",
+    "Connection": "keep-alive",
+    "Upgrade-Insecure-Requests": "1",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "same-origin",
+    "Sec-Fetch-User": "?1",
+    "Priority": "u=0, i",
+    "TE": "trailers",
     "cookie": (
         "JSESSIONID_TEMCHRONOWEB=E4309B40987CA52F8FD21082823DE7DD.te-mchronowebi2-NODE7; "
         "lang=fr_FR; "
-        "JSESSIONID_WSREST=.tc-mchronoweb-NODE5; "
-        "SAWWID=.NODE14; "
         "parcours=Professionnel; "
         "parcoursId=4; "
         f"iv4={iv4}; "
-        "jur=4; "
-        "connect=OUI; "
-        "cookie_profile_role=4; "
-        "WebProRef=W2919011; "
-        "cookie_ie6=true; "
-        "cookie_idSessionShipping=21BB3B71BB4E7510C60EF65F7A1C7F93.te-mchronowebi2-NODE10; "
-        "cookie_nbCartArticles=0; "
-        "JSESSIONID_WSSHIPPING=63B95F899A0A8B4C71646B8AAE1243A5.tc-wsshipping-NODE14; "
-        "cookie_idCart=294580485; "
-        "cookie_lastCartUpdate=27/01/2026/21:21; "
-        "cf_clearance=S5tM260WCJR9K9lsxtrxnF2HtLOZDBK6t5_aZt1JyXY-1769546260-1.2.1.1-3Pw6hQ_9LnPIo_Tk_va7YeLWYTK7lawH0iaGd0Gq9EKkvLPogQbJCXEvR83_GZCa0Er6EPdihlBkd9oBxDC5JS1auE4hEj.l7kB1lIe8JW0AG5Z8DpvZD18WIb.rBJBN13VAExmWKjT2JvDCOCDbEUlctf9aL2J6vVWGoxIMKptJSwQHIn_HTtFgxPx9UT3d4iE24LPJx22Lu9TbBng5Sf_9ZgkGUc30VIHtcspuYnU; "
-        "__cf_bm=Eh0b53hkup1RwsWIURRgMa4UID8TWjjM6wZFexcoQt8-1769546260-1.0.1.1-2SyvTuB0AhUS_BcPOy2eqfYO93GD8FZo_NLB..fpjiLpKmjmrazy1GfRp57sFRs6XOtAUNEe6.ZpF3V3S1puSOgTsWeSbVOdZNSAKdkPsy4"
-    ),
+        "pa_privacy=%22exempt%22"
+        ),
 }
 
 HEADERS_2 = {
-    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-    "accept-encoding": "gzip, deflate, br, zstd",
-    "accept-language": "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7",
-    "priority": "u=0, i",
-    "referer": f"https://www.chronopost.fr/moncompte/displayCustomerArea.do?iv4Context={iv4}&lang=fr_FR",
-    "sec-ch-ua": "\"Not(A:Brand\";v=\"8\", \"Chromium\";v=\"144\", \"Google Chrome\";v=\"144\"",
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": "\"Windows\"",
-    "sec-fetch-dest": "document",
-    "sec-fetch-mode": "navigate",
-    "sec-fetch-site": "same-origin",
-    "sec-fetch-user": "?1",
-    "upgrade-insecure-requests": "1",
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+    "Accept-Language": "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
+    "Upgrade-Insecure-Requests": "1",
+    "Priority": "u=0, i",
     "cookie": (
         "JSESSIONID_TEMCHRONOWEB=21BB3B71BB4E7510C60EF65F7A1C7F93.te-mchronowebi2-NODE10; "
-        "cookie_profileSession=2918607; "
         "lang=fr_FR; "
-        "JSESSIONID_WSREST=.tc-mchronoweb-NODE5; "
-        "SAWWID=.NODE14; "
-        "parcours=Professionnel; "
-        "parcoursId=4; "
-        f"iv4={iv4}; "
-        "jur=4; "
-        "connect=OUI; "
-        "cookie_profile_role=4; "
-        "WebProRef=W2919011; "
-        "cookie_ie6=true; "
-        "cookie_idSessionShipping=21BB3B71BB4E7510C60EF65F7A1C7F93.te-mchronowebi2-NODE10; "
-        "cookie_nbCartArticles=0; "
-        "JSESSIONID_WSSHIPPING=63B95F899A0A8B4C71646B8AAE1243A5.tc-wsshipping-NODE14; "
-        "cookie_idCart=294580485; "
-        "cookie_lastCartUpdate=27/01/2026/21:21; "
-        "cf_clearance=S5tM260WCJR9K9lsxtrxnF2HtLOZDBK6t5_aZt1JyXY-1769546260-1.2.1.1-3Pw6hQ_9LnPIo_Tk_va7YeLWYTK7lawH0iaGd0Gq9EKkvLPogQbJCXEvR83_GZCa0Er6EPdihlBkd9oBxDC5JS1auE4hEj.l7kB1lIe8JW0AG5Z8DpvZD18WIb.rBJBN13VAExmWKjT2JvDCOCDbEUlctf9aL2J6vVWGoxIMKptJSwQHIn_HTtFgxPx9UT3d4iE24LPJx22Lu9TbBng5Sf_9ZgkGUc30VIHtcspuYnU; "
-        "__cf_bm=zvxPCkGcMY745EKfTayv9BV.6hovM0d0xRn_03EnYVE-1769546280.3111167-1.0.1.1-KyyPFiUBbB0OGQkF3Pl5KwXYgFuGdfoU34X9fc0S6.x4Iy5WG2cmgH2y54wOajkWfuTV.GR.XsCk_IekPTkkX3RUovi.bK8mM7UG6Z9aBd003jjoddO_2NbYegIRWKh8"
-    ),
+        f"iv4={iv4}"
+        ),
 }
 
 HEADERS_4 = {
@@ -93,7 +53,6 @@ HEADERS_4 = {
     "sec-fetch-user": "?1",
     "upgrade-insecure-requests": "1",
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36",
-
     "cookie": (
         "W0827094_40859_87668902=17; "
         "W1410789_3100_25322901=17; "
