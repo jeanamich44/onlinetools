5a811cacf094b243063c9055f1b72a3336e6f795
TonNom <tonemail@email.com>
2026-01-27 21:31:04 +0100

    kljd$

--- chronopost/script/headers.py
+++ chronopost/script/headers.py
@@ -1,23 +1,28 @@
 HEADERS_1 = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:146.0) Gecko/20100101 Firefox/146.0",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-    "Accept-Language": "fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3",
-    "Accept-Encoding": "gzip, deflate, br, zstd",
-    "Referer": "https://www.chronopost.fr/fr",
-    "Connection": "keep-alive",
-    "Upgrade-Insecure-Requests": "1",
-    "Sec-Fetch-Dest": "document",
-    "Sec-Fetch-Mode": "navigate",
-    "Sec-Fetch-Site": "same-origin",
-    "Sec-Fetch-User": "?1",
-    "Priority": "u=0, i",
-    "TE": "trailers",
-    "Cookie": (
-        "JSESSIONID_TEMCHRONOWEB=40D00C35757676A897A2E6EB4810354F.te-mchronowebi2-NODE13; "
-        "pa_privacy=%22exempt%22; lang=fr_FR; parcours=Professionnel; parcoursId=4; "
-        "iv4=cb9ad1180a51ecc2e7cbf3e83c343581"
-    )
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "accept-encoding": "gzip, deflate, br, zstd",
+    "accept-language": "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7",
+    "cookie": (
+        "JSESSIONID_TEMCHRONOWEB=E4309B40987CA52F8FD21082823DE7DD.te-mchronowebi2-NODE7; "
+        "parcours=Professionnel; "
+        "pa_privacy=%22exempt%22; "
+        "parcoursId=4; "
+        "lang=fr_FR; "
+        "iv4=d8731416d5d60aac657dd0120cc49f59"
+    ),
+    "priority": "u=0, i",
+    "referer": "https://www.chronopost.fr/fr",
+    "sec-ch-ua": "\"Not(A:Brand\";v=\"8\", \"Chromium\";v=\"144\", \"Google Chrome\";v=\"144\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-site": "same-origin",
+    "sec-fetch-user": "?1",
+    "upgrade-insecure-requests": "1",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36",
 }
+
 
 HEADERS_2 = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36",
@@ -27,27 +32,27 @@ HEADERS_2 = {
     "Upgrade-Insecure-Requests": "1",
     "Priority": "u=0, i",
     "Cookie": (
-        "JSESSIONID_TEMCHRONOWEB=63CC995BD09391BB290BB9AF86C6E4D3.te-mchronowebi2-NODE14; "
-        "iv4=47220cd6138831e87b89bc06aa246440; lang=fr_FR"
+        "JSESSIONID_TEMCHRONOWEB=E4309B40987CA52F8FD21082823DE7DD.te-mchronowebi2-NODE7; "
+        "lang=fr_FR; iv4=d8731416d5d60aac657dd0120cc49f59"
     )
 }
 
 HEADERS_4 = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-    "Accept-Language": "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7",
-    "Accept-Encoding": "gzip, deflate, br, zstd",
-    "Referer": "https://www.chronopost.fr/moncompte/displayCustomerArea.do?iv4Context=47220cd6138831e87b89bc06aa246440&lang=fr_FR",
-    "Upgrade-Insecure-Requests": "1",
-    "Priority": "u=0, i",
-    "Sec-Fetch-Dest": "document",
-    "Sec-Fetch-Mode": "navigate",
-    "Sec-Fetch-Site": "same-origin",
-    "Sec-Fetch-User": "?1",
-    "Sec-CH-UA": "\"Google Chrome\";v=\"143\", \"Chromium\";v=\"143\", \"Not A(Brand\";v=\"24\"",
-    "Sec-CH-UA-Mobile": "?0",
-    "Sec-CH-UA-Platform": "\"Windows\"",
-    "Cookie": (
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "accept-encoding": "gzip, deflate, br, zstd",
+    "accept-language": "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7",
+    "priority": "u=0, i",
+    "referer": "https://www.chronopost.fr/moncompte/displayCustomerArea.do?iv4Context=d8731416d5d60aac657dd0120cc49f59&lang=fr_FR",
+    "sec-ch-ua": "\"Not(A:Brand\";v=\"8\", \"Chromium\";v=\"144\", \"Google Chrome\";v=\"144\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-site": "same-origin",
+    "sec-fetch-user": "?1",
+    "upgrade-insecure-requests": "1",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36",
+    "cookie": (
         "W0827094_40859_87668902=17; "
         "W1410789_3100_25322901=17; "
         "W2791128_24938_52070101=17; "
@@ -56,22 +61,25 @@ HEADERS_4 = {
         "W1029613_47107_15972103=1; "
         "W1029613__=1; "
         "W2919011_47107_15972103=1; "
-        "CookieConsent={stamp:%27BHeWjFkHeZq60sjshypy3dYCVhD/w1W9isAkkB+WsG2aLdgi3E3arw==%27%2Cnecessary:true%2Cpreferences:true%2Cstatistics:true%2Cmarketing:true%2Cmethod:%27explicit%27%2Cver:1%2Cutc:1762971035855%2Cregion:%27fr%27}; "
-        "_gcl_au=1.1.1289856221.1765118666; "
-        "iv4=47220cd6138831e87b89bc06aa246440; "
-        "cookie_profile_role=4; "
-        "WebProRef=W2919011; "
-        "cookie_nbCartArticles=0; "
+        "numeroLT=XN157477444FR; "
+        "fileDownloadToken=1769544610546; "
         "lang=fr_FR; "
-        "JSESSIONID_WSREST=.tc-mchronoweb-NODE12; "
+        "JSESSIONID_WSREST=.tc-mchronoweb-NODE5; "
+        "SAWWID=.NODE14; "
         "parcours=Professionnel; "
         "parcoursId=4; "
-        "SAWWID=.NODE11; "
+        "cf_clearance=HGk_Mpy8tOYi35VqPdV.xQ5.6sUIoROLOoM0R7T.lFE-1769544343-1.2.1.1-gy1i_AaJEEloEB3LNJ9hD13WARFZI_P9.jOXGBwbhh47gwGeKjL7Rs.IuIrlbIVZE4uh1MWlvIl.r4jhxB6xEe0mrrxuaDJFIhnAzdKY7zeqWZJJx.a9LSZDwoR4gAnQeR8MiciysY6oilPdMkJoor.ICrjEdOmQtPzzrXXu528MPt8_CHcNFqItB7rOFvH3bW8qeidnmaA3ZZzLQwU.kD58N7e5SOmVOShxWEWw5cY; "
+        "iv4=d8731416d5d60aac657dd0120cc49f59; "
+        "jur=4; "
+        "connect=OUI; "
+        "cookie_profile_role=4; "
+        "WebProRef=W2919011; "
         "cookie_ie6=true; "
-        "cookie_idSessionShipping=0CE79C74FA3CAA0F064B22152107E20E.te-mchronowebi2-NODE10; "
-        "cookie_idCart=293804693; "
-        "JSESSIONID_WSSHIPPING=8452C3CF0B62E68A86CEC9DA134BCBCB.tc-wsshipping-NODE1; "
-        "cookie_lastCartUpdate=18/01/2026/01:36; "
-        "__cf_bm=26qiK1IqIdepVYuunAmlvHIJea0e7H.Np6f7UpQhb_0-1768696584.124139-1.0.1.1-eZRikA96ZOxK4wSfnYUH7cSH85HkjTjRmgKCldvbj4vjnPm_2smkfcCtOhTO7awMImTBaMMh4vaZhzqCFRKgfPHesIIUC3Kh7_LIusAnRKpm7FbCOwwbM3LpQhaBoavN"
-    )
+        "cookie_idSessionShipping=21BB3B71BB4E7510C60EF65F7A1C7F93.te-mchronowebi2-NODE10; "
+        "cookie_nbCartArticles=0; "
+        "JSESSIONID_WSSHIPPING=63B95F899A0A8B4C71646B8AAE1243A5.tc-wsshipping-NODE14; "
+        "cookie_idCart=294580485; "
+        "cookie_lastCartUpdate=27/01/2026/21:21; "
+        "__cf_bm=7eQk9jGPnCr8MKobEjbm.2Kc2.Pbu38DznKXqxQWQYU-1769545289.8480744-1.0.1.1-d.ZHw3.GzPDkN24FIfnAPQ_ogvWuWihORNm5Qi3UnKj.iGnqLmD9CZQk8vBd45ZhA93L4PhZDuzDS0Wq39Ib5iJW6YlGjqweXMW2E_6InwOtxeNh0z3csRxt7OLDL2Dm"
+    ),
 }
