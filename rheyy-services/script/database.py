1e5bc358603276f4a50941dbd9140afc66c01f69
TonNom <tonemail@email.com>
2026-04-15 20:50:52 +0200

    fix

--- rheyy-services/script/database.py
+++ rheyy-services/script/database.py
@@ -1,6 +1,6 @@
 import os
 from datetime import datetime
-from sqlalchemy import create_engine, Column, Integer, String, Float, DateTime, Boolean, Text
+from sqlalchemy import create_engine, Column, Integer, String, Float, DateTime, Boolean, Text, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import logging
